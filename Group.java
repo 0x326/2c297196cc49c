fd504c62a042a303121f048fb8c26f395a39fead
clemenbr <clemenbr@miamioh.edu>
2016-10-31 19:26:44 -0400

    declare variable

--- Group.java
+++ Group.java
@@ -1,7 +1,10 @@
 public class Group {
-  public static void main(String[] args) {
-    // Declare variables
-    
+  
+   public static void main(String[] args) {
+      
+   // Declare variables
+   int area = 20; 
+   
     //Do stuff
     
     System.out.println("area");
