88e3ac1e59e7734165161e9426374faa7d31eb9d
John Meyer <jmm2016@ymail.com>
2016-10-31 20:05:12 -0400

    Update Group.java

--- Group.java
+++ Group.java
@@ -1,9 +1,22 @@
 public class Group {
   public static void main(String[] args) {
     // Declare variables
+    int[] arr;
+    int a = 5;
+    int b = 6;
+    int c = 9;
+    int[] num = {5,5,6,2,5,2};
+    
     
     //Do stuff
     
     System.out.println(area);
+    for (int i =adsajkfljl) {
+    
+      // test
+      // if meets parameters
+      num
+    }
+    10 % 3 == 0;
   }
 }
