c71d24969235508f09caba48cbb21a3fc5c1281b
John Meyer <jmm2016@ymail.com>
2016-10-31 21:14:27 -0400

    Add Problem 551
    Initialize problem 551. This should be relatively easy.

---
+++ Problem551.java
@@ -0,0 +1,12 @@
+// Name: John Meyer, Bri Clements, Noah Sheridan, Tianyu Zhang, Ranyang Hu
+// Instructor: Vijayalakshmi Ramasamy
+// CSE 174, Section A
+// Date: October 31, 2016
+// Filename: Problem551.java
+// Description: Solves problem at https://projecteuler.net/problem=551
+
+public class Problem551 {
+  public static void main(String[] args) {
+    ;
+  }
+}
