42de735285e8e72815d73d80936006db102bdecc
John Meyer <jmm2016@ymail.com>
2016-10-31 20:19:00 -0400

    Clean up Problem 1

--- problem1.java
+++ problem1.java
@@ -1,13 +1,21 @@
-public class problem1 {
-  public static void main(String[] args){
-    int sum = 0;
+// Name: John Meyer, Bri Clements, Noah Sheridan, Tianyu Zhang, Ranyang Hu
+// Instructor: Vijayalakshmi Ramasamy
+// CSE 174, Section A
+// Date: October 31, 2016
+// Filename: Problem1.java
+// Description: Solves problem one at https://projecteuler.net/
+
+public class Problem1 {
+  public static void main(String[] args) {
+    // Declare Variables
+    int sumOfFactors = 0;
+    int numLimit = 1000;
     
-    for(int currentNum=0;currentNum<1000;currentNum++){
-    
+    // Search through all numbers within range
+    for(int currentNum = 0; currentNum < numLimit; currentNum++) {
+      // If it is a factor of 3 or 5, add it to the sum
       if(currentNum % 3 == 0 || currentNum % 5 == 0) {
-        sum += currentNum;
-      
-      
+        sumOfFactors += currentNum;
       }
     }
     System.out.println(sum);
