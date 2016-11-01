038ef240ba776005ac923c16af4ad2981aa45f0e
John Meyer <jmm2016@ymail.com>
2016-10-31 20:53:18 -0400

    Add stub for Problem 17

---
+++ Problem17.java
@@ -0,0 +1,21 @@
+// Name: John Meyer, Bri Clements, Noah Sheridan, Tianyu Zhang, Ranyang Hu
+// Instructor: Vijayalakshmi Ramasamy
+// CSE 174, Section A
+// Date: October 31, 2016
+// Filename: Problem17.java
+// Description: Solves problem at https://projecteuler.net/problem=17
+
+public class Problem17 {
+  public static void main(String[] args) {
+    // Declare Variables
+    int numToCompute;
+    
+    // Do computation
+    switch (num) {
+      case 1:
+        "one";
+        return;
+      case 2:
+        "two";
+    
+    // Display result
---
+++ Problem17.md
@@ -0,0 +1,11 @@
+Problem 17
+==========
+
+We know that:
+
+ - 1 -> "one"
+ - 2 -> "two"
+ - 3 -> "three"
+ - and so forth
+ - 20 -> "twenty"
+ - 21 -> "twenty one" (String of `20` + String of `1`, separated by a space)
