ed75b13597017b143a181efa2fa20e3b6a3552a6
John Meyer <jmm2016@ymail.com>
2016-11-11 18:34:17 -0500

    Add class for final presentation
    Create a structure such that we can submit a executable .jar file

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * This is the class we will use for our presentation of the project
+ * It will call the methods of the individual problem files such as `Problem47.main()`
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Foo");
+  }
+}
