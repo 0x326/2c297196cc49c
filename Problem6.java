b50aafdeb8311e802fb38c57efccd096bd9cf7ed
John Meyer <jmm2016@ymail.com>
2016-11-17 21:53:44 -0500

    Clarify variable names

--- Problem6.java
+++ Problem6.java
@@ -9,15 +9,15 @@ public class Problem6 {
   public static void main(String[] args) {
     int sumOfSquares = 0;
     int squareOfSums = 0;
-    int sum = 0;
+    int sumOfIntegers = 0;
     for (int num = 0; num <= 100; num++) {
       sumOfSquares += Math.pow(num, 2);
     }
     
-    for (int num2 = 0; num2 <= 100; num2++) {
-      sum += num2;
+    for (int num = 0; num <= 100; num++) {
+      sumOfIntegers += num;
     }
-    squareOfSums = (int)(Math.pow(sum, 2));
+    squareOfSums = (int)(Math.pow(sumOfIntegers, 2));
     
     System.out.println(squareOfSums - sumOfSquares); 
   }
