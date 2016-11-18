ffa41ec6b677345f8665eab171c683d4b1abdbee
John Meyer <jmm2016@ymail.com>
2016-11-17 21:51:15 -0500

    Add boiler plate to files

--- Problem124.java
+++ Problem124.java
@@ -1,3 +1,10 @@
+// Name: John Meyer, Bri Clements, Noah Sheridan, Tianyu Zhang, Ranyang Hu
+// Instructor: Vijayalakshmi Ramasamy
+// CSE 174, Section A
+// Date: November 11, 2016
+// Filename: Problem124.java
+// Description: Solves problem at https://projecteuler.net/problem=124
+
 public class Problem124 {
   public static void main(String[] args) {
     ;
--- Problem26.java
+++ Problem26.java
@@ -1,3 +1,10 @@
+// Name: John Meyer, Bri Clements, Noah Sheridan, Tianyu Zhang, Ranyang Hu
+// Instructor: Vijayalakshmi Ramasamy
+// CSE 174, Section A
+// Date: November 11, 2016
+// Filename: Problem26.java
+// Description: Solves problem at https://projecteuler.net/problem=26
+
 public class Problem26 {
   public static void main(String[] args) {
     for (double d = 2; d < 10; d++) {
