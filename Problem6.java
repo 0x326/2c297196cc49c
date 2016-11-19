512586260d2eec3a14ae54542c527030bb8f2998
John Meyer <jmm2016@ymail.com>
2016-11-19 14:14:02 -0500

    Restructure problem

--- Problem6.java
+++ Problem6.java
@@ -7,18 +7,20 @@
 
 public class Problem6 {
   public static void main(String[] args) {
+    System.out.println(differenceOperation(100));
+  }
+  public static int differenceOperation(int limit) {
     int sumOfSquares = 0;
-    int squareOfSums = 0;
-    int sumOfIntegers = 0;
-    for (int num = 0; num <= 100; num++) {
+    for (int num = 0; num <= limit; num++) {
       sumOfSquares += Math.pow(num, 2);
     }
     
-    for (int num = 0; num <= 100; num++) {
+    int sumOfIntegers = 0;
+    for (int num = 0; num <= limit; num++) {
       sumOfIntegers += num;
     }
-    squareOfSums = (int)(Math.pow(sumOfIntegers, 2));
+    int squareOfSums = (int)(Math.pow(sumOfIntegers, 2));
     
-    System.out.println(squareOfSums - sumOfSquares); 
+    return squareOfSums - sumOfSquares; 
   }
 }
