7356f63c2c8164fd635fbd75482594e0317457b3
John Meyer <jmm2016@ymail.com>
2016-11-29 23:14:30 -0500

    Add sample (boilerplate) file for convenience

---
+++ Boilerplates/ProjectEulerProblem.java
@@ -0,0 +1,12 @@
+// Name: John Meyer, Bri Clements, Noah Sheridan, Tianyu Zhang, Ranyang Hu
+// Instructor: Vijayalakshmi Ramasamy
+// CSE 174, Section A
+// Date: <<<DATE>>>
+// Filename: <<<NAME>>>.java
+// Description: Solves problem at https://projecteuler.net/problem=<<<PROBLEM_NUMBER>>>
+
+public class <<<NAME>>> {
+  public static void main(String[] args) {
+    ;
+  }
+}
---
+++ Boilerplates/README.md
@@ -0,0 +1,2 @@
+# For Project Euler Problems
+Use the given boilerplate and replace the given items
